6231257f89cac92c94015cb208d5eb9225fce1f7
Sam <samharaway33@gmail.com>
2025-07-27 15:03:27 +0900

    navbar and file structure Linked

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -3,10 +3,13 @@ import Link from "next/link";
 
 export const Navbar = () => {
     return (
-        <div>
-            <Link href="./project-portfolio"> Projects </Link>
-            <Link href="./about">About </Link>
-            <Link href="./contact">Contact</Link>
+        <div className="flex w-full h-16 items-center bg-gray-800/50 justify-center">
+            <div className="flex justify-between w-1/2 h-10 items-center p-4">
+                <Link href="../app">Home</Link>
+                <Link href="./project-portfolio"> Projects </Link>
+                <Link href="./about">About </Link>
+                <Link href="./contact">Contact</Link>
+            </div>
         </div>
     )
 }
