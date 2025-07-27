0a42a829fe43065f0ba6912593faa8e090c6a9c7
Sam <samharaway33@gmail.com>
2025-07-27 16:11:08 +0900

    basic home page setup

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -5,10 +5,10 @@ export const Navbar = () => {
     return (
         <div className="flex w-full h-16 items-center bg-gray-800/50 justify-center">
             <div className="flex justify-between w-1/2 h-10 items-center p-4">
-                <Link href="/">Home</Link>
-                <Link href="/project-portfolio"> Projects</Link>
-                <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
+                <Link href="/">home</Link>
+                <Link href="/project-portfolio">projects</Link>
+                <Link href="/about">about</Link>
+                <Link href="/contact">contact</Link>
             </div>
         </div>
     )
